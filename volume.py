82dae13b37b52d9c0b5fe5e9cfe04ee56a4fa9e3
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-05-20 16:34:36 +0000

    “volume”

---
+++ volume.py
@@ -0,0 +1,34 @@
+# /bin/env python3
+# Created by: Enoch O
+# Created on: May 14, 2025
+# This program shall calculate/find the volume of a cube by obtaining its side.
+
+
+def calculate_volume(side_length):
+    return side_length**3
+
+
+# Main function to get user input and display results
+def main():
+    while True:
+        side_length_str = input("Enter the length of your cube (cm): ") #getting length as str
+
+        try:
+            side_length = float(side_length_str) # Turning the string to a float
+
+            if side_length > 0 and side_length <= 1000:
+                volume = calculate_volume(side_length)
+                print(
+                    f"The volume of the cube with side length {side_length} cm is {volume} cubic cm."
+                ) # The result of the codes calculations
+                break  #  Exit the loop after  calculation
+            else:
+                print("Error: Side length must be a between 0 and 1000 cm!!!") 
+
+        except ValueError:
+            print("Invalid input. Please enter a positive number between 0 and 1000.")
+
+
+# Run the program
+if __name__ == "__main__":
+    main()
